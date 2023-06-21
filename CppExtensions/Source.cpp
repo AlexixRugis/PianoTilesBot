2dfe4e41ab425c366630f163066c7453bccc158d
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2023-06-21 15:23:16 +0300

    fixed tabs

--- CppExtensions/Source.cpp
+++ CppExtensions/Source.cpp
@@ -7,29 +7,29 @@ extern "C" __declspec(dllexport) unsigned char* take_screenshot(int x, int y, in
 	HBITMAP bmpTarget = CreateCompatibleBitmap(dcScreen, width, height);
 	HGDIOBJ oldBmp = SelectObject(dcTarget, bmpTarget);
 	BitBlt(dcTarget, 0, 0, width, height, dcScreen, x, y, SRCCOPY);
-
+	
     BITMAP Bmp = { 0 };
     BITMAPINFO Info = { 0 };
     GetObject(bmpTarget, sizeof(Bmp), &Bmp);
-
-    Info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    Info.bmiHeader.biWidth = width = Bmp.bmWidth;
-    Info.bmiHeader.biHeight = height = Bmp.bmHeight;
-    Info.bmiHeader.biPlanes = 1;
-    Info.bmiHeader.biBitCount = Bmp.bmBitsPixel;
-    Info.bmiHeader.biCompression = BI_RGB;
-    Info.bmiHeader.biSizeImage = ((width * Bmp.bmBitsPixel + 31) / 32) * 4 * height;
-
-    unsigned char* pixels = new unsigned char[Info.bmiHeader.biSizeImage];
-    GetDIBits(dcTarget, bmpTarget, 0, height, pixels, &Info, DIB_RGB_COLORS);
-
+	
+	Info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	Info.bmiHeader.biWidth = width = Bmp.bmWidth;
+	Info.bmiHeader.biHeight = height = Bmp.bmHeight;
+	Info.bmiHeader.biPlanes = 1;
+	Info.bmiHeader.biBitCount = Bmp.bmBitsPixel;
+	Info.bmiHeader.biCompression = BI_RGB;
+	Info.bmiHeader.biSizeImage = ((width * Bmp.bmBitsPixel + 31) / 32) * 4 * height;
+	
+	unsigned char* pixels = new unsigned char[Info.bmiHeader.biSizeImage];
+	GetDIBits(dcTarget, bmpTarget, 0, height, pixels, &Info, DIB_RGB_COLORS);
+	
 	SelectObject(dcTarget, oldBmp);
 	DeleteObject(bmpTarget);
 	DeleteObject(oldBmp);
 	DeleteDC(dcTarget);
 	DeleteDC(dcScreen);
 
-    return pixels;
+	return pixels;
 }
 
 extern "C" __declspec(dllexport) void freep(unsigned char* p) {
